5ccc797809bdfd866ae5e401e811854450ffa441
az-z <formisc@gmail.com>
2017-04-27 22:36:09 -0400

    The logic for Vent has been added. The constructor was added. The time is still in seconds for debuging purposes.

--- RF_Test/Transmit/src/transmit.cpp
+++ RF_Test/Transmit/src/transmit.cpp
@@ -9,7 +9,7 @@ const bool OFF = false;
 // Why i can't move these 2 lines into the class?
 // Actually a normal person would inherit this class and add timings as i do
 // with Socket_t
-const int DATA_PIN = 4;
+const int8_t DataPin = 4;
 // Outlet outlet(DATA_PIN); //  make a new outlet instance with
 
 class Socket_t : public Outlet {
@@ -24,35 +24,39 @@ protected:
   // unsigned long const DurationMillSecs = 50000;//43200000; //12*60*60*1000
   // duration of the 12 hours
   // on / 12 hours off cycle in milisecs
-  unsigned long prevMillSecs; // the previous value of the call to mills()
+  unsigned long PrevMillSecs; // the previous value of the call to mills()
 public:
-  Socket_t(int ch, bool state, int8_t Hour, DATA_PIN) {
+  Socket_t(int ch, bool state, int8_t Hour, int DPin) : Outlet(DPin) {
     Channel = ch;
     State = state;
-    Outlet::Outlet(DATA_PIN);
     toggle(Channel, State);
     DurationMillSecs = Hour * 60000; // in minutes for testing
-    prevMillSecs = 0;
+    PrevMillSecs = 0;
   };
+  void SetState(bool St) {
+    State = St;
+    toggle(Channel, State);
+  };
+  bool getState() { return State; };
   void Update(unsigned long currMills) {
-    if (currMills - prevMillSecs >= DurationMillSecs) {
+    if (currMills - PrevMillSecs >= DurationMillSecs) {
       State = (State == ON)
                   ? OFF
                   : ON; // Switch the State after current time is over Dur
       toggle(Channel, State);
-      prevMillSecs = currMills;
+      PrevMillSecs = currMills;
     };
   };
-  bool getState() { return State; };
 };
 
-Socket_t Lamp(1, ON, 1);
-Socket_t Vent(2, ON, 2);
+Socket_t Lamp(1, ON, 3, DataPin);
+Socket_t Vent(2, ON, 1, DataPin);
+long timestop;
 
 void setup() {
   pinMode(ledPin, HIGH);
   Serial.begin(9600);
-  //  Serial.println("Setup");
+  timestop = 0;
   /*
      Here i'll magically setup the Sockets per channel. For now i pair them
      before runing this program
@@ -60,11 +64,15 @@ void setup() {
 }
 
 void loop() {
-  pinMode(ledPin, HIGH);
-  Serial.println("Waiting 10 secs");
+  // pinMode(ledPin, HIGH);
   delay(10000);
-  // Lamp.Update(millis());
-  // if (Lamp.getState()) { // turn Vent only if the Lamp is on
-  //   Vent.Update(millis());
-  // }
+  timestop += 10000;
+  Serial.print("Waited : ");
+  Serial.print(timestop / 1000);
+  Serial.println(" s");
+  Lamp.Update(millis());
+  if (Lamp.getState()) { // turn Vent only if the Lamp is on
+    Vent.Update(millis());
+  } else
+    Vent.SetState(OFF);
 }
